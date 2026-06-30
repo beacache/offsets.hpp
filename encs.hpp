ed9b96bc344f3385f579cfd4a1921d975a9ed77e
sc <cachebea@gmail.com>
2026-06-30 18:45:42 +0300

    Update encs.hpp

--- encs.hpp
+++ encs.hpp
@@ -21,7 +21,11 @@
 #define proto_typeinfo     vmvalue3/* proto_typeinfo_enc */
 #define proto_abslineinfo  vmvalue1/* proto_abslineinfo_enc */
 #define proto_source       vmvalue1/* proto_source_enc */
+#define proto_locvars      vmvalue0/* proto_locvars_enc */
+#define proto_upvalues     vmvalue0/* proto_upvalues_enc */
+#define proto_debugname    vmvalue0/* proto_debugname_enc */
+#define proto_userdata     vmvalue0/* proto_userdata_enc */
 #define udata_meta         vmvalue1/* udata_meta_enc */
-#define closure_debugname  vmvalue0/* closure_debugname_enc */
+#define closure_debugname  vmvalue4/* closure_debugname_enc */
 #define closure_cont       vmvalue0/* closure_cont_enc */
 #define lstate_stacksize   vmvalue3/* lstate_stacksize_enc */
