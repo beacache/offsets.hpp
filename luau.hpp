f4f941093ae04e2743c68c04573f498cfea3387b
sc <cachebea@gmail.com>
2026-07-02 14:52:10 +0300

    Update and rename encs.hpp to luau.hpp

--- luau.hpp
+++ luau.hpp
@@ -5,8 +5,9 @@
 #pragma warning(disable: 4172)
 
 // dumped by sc(@k1ci0)
-// roblox version: version-1a951716f19e4638
+// roblox version: version-5cf2272675e145f5
 
+// dumped in 147432ms (147.432s)
 /*
               SET                                 GET
     vmvalue0: data = value                     || value = data
@@ -16,16 +17,16 @@
     vmvalue4: data = (value - (data + offset)) || value = ((v + offset) + data)
 */
 
-#define proto_lineinfo     vmvalue1/* proto_lineinfo_enc */
-#define proto_debuginsn    vmvalue4/* proto_debuginsn_enc */
-#define proto_typeinfo     vmvalue3/* proto_typeinfo_enc */
-#define proto_abslineinfo  vmvalue1/* proto_abslineinfo_enc */
+#define proto_lineinfo     vmvalue0/* proto_lineinfo_enc */
+#define proto_debuginsn    vmvalue2/* proto_debuginsn_enc */
+#define proto_typeinfo     vmvalue1/* proto_typeinfo_enc */
+#define proto_abslineinfo  vmvalue3/* proto_abslineinfo_enc */
 #define proto_source       vmvalue1/* proto_source_enc */
 #define proto_locvars      vmvalue0/* proto_locvars_enc */
 #define proto_upvalues     vmvalue0/* proto_upvalues_enc */
 #define proto_debugname    vmvalue0/* proto_debugname_enc */
 #define proto_userdata     vmvalue0/* proto_userdata_enc */
-#define udata_meta         vmvalue1/* udata_meta_enc */
+#define udata_meta         vmvalue0/* udata_meta_enc */
 #define closure_debugname  vmvalue4/* closure_debugname_enc */
-#define closure_cont       vmvalue0/* closure_cont_enc */
+#define closure_cont       vmvalue4/* closure_cont_enc */
 #define lstate_stacksize   vmvalue3/* lstate_stacksize_enc */
